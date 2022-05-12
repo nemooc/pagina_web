6cc7e30c796a45315f55d5137bedb3f59c2a147a
nemooc <92552396+nemooc@users.noreply.github.com>
2022-05-12 13:03:19 -0300

    AA

--- config/env.php
+++ config/env.php
@@ -1,6 +1,6 @@
 ﻿<?php
 //Servidor de base de datos
-define("DB_HOST", "localhost:3307");
+define("DB_HOST", "localhost:3306");
 
 //Nombre de la base de datos
 define("DB_NAME", "sistema");
--- models/Producto.php
+++ models/Producto.php
@@ -5,12 +5,19 @@ class Producto
 {
     public function __construct()
     {
+
     }
+
     public static function traerProductos ()
     {
     $sql = "SELECT * FROM productos";
     return ejecutarConsulta($sql);
     
     }
+
+    public static function traer_Categorias ()
+    {
+    $sql = "SELECT categoria FROM productos GROUP BY categoria";
+    return ejecutarConsulta($sql);
    
-}
+    }
